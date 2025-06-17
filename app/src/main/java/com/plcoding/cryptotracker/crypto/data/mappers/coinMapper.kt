1b5620a906a9810095e314142da8cc516106227c
bilalazzam <blalazam065@gmail.com>
2025-06-17 20:04:14 +0300

    Initial commit

---
+++ app/src/main/java/com/plcoding/cryptotracker/crypto/data/mappers/coinMapper.kt
@@ -0,0 +1,29 @@
+package com.plcoding.cryptotracker.crypto.data.mappers
+
+import com.plcoding.cryptotracker.crypto.data.dto.CoinDto
+import com.plcoding.cryptotracker.crypto.data.dto.CoinPriceDto
+import com.plcoding.cryptotracker.crypto.domain.Coin
+import com.plcoding.cryptotracker.crypto.domain.CoinPrice
+import java.time.Instant
+import java.time.ZoneId
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id = this.id,
+        rank = this.rank,
+        name = this.name,
+        symbol = this.symbol,
+        marketCapUsd = this.marketCapUsd,
+        priceUsd = this.priceUsd,
+        changePercent24Hr = this.changePercent24Hr
+    )
+}
+
+fun CoinPriceDto.toCoinPrice(): CoinPrice {
+    return CoinPrice(
+        priceUsd = priceUsd,
+        dateTime = Instant
+            .ofEpochMilli(time)
+            .atZone(ZoneId.of("UTC"))
+    )
+}
